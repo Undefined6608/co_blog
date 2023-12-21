85b93a825ab531a93693c4b4ce40640ff2ad97ae
15506 <15506531157@163.com>
2023-12-21 20:51:35 +0800

    vercel-proxy

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,17 +1,22 @@
-// 该服务为 vercel serve跨域处理
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-	let target = "";
+	let target = ''
 
-	if (req.url.startsWith("/api")) {
-		// 这里填目标地址
-		target = "http://137.175.84.209:6005";
+	// 代理目标地址
+	// 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+	if (req.url.startsWith('/api')) {
+		target = 'http://137.175.84.209'
 	}
+
 	// 创建代理对象并转发请求
 	createProxyMiddleware({
 		target,
-		changeOrigin: true
-	})(req, res);
-};
-
+		changeOrigin: true,
+		pathRewrite: {
+			// 通过路径重写，去除请求路径中的 `/backend`
+			// 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+			'^/api/': '/api/'
+		}
+	})(req, res)
+}
